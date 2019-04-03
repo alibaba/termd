1b8238baecfa15ca5206960a420db68a32f3e08d
hengyunabc <hengyunabc@gmail.com>
2019-04-03 21:10:00 +0800

    improve HistorySearchBackward HistorySearchForward. alibaba/arthas#560

--- src/main/java/io/termd/core/readline/functions/HistorySearchBackward.java
+++ src/main/java/io/termd/core/readline/functions/HistorySearchBackward.java
@@ -27,17 +27,40 @@ public class HistorySearchBackward implements Function {
 
         int curr = interaction.getHistoryIndex();
 
-        int searchStart = curr + 1;
-
-        for (int i = searchStart; i < history.size(); ++i) {
-            int[] line = history.get(i);
-            if (LineBufferUtils.equals(buf, line)) {
-                continue;
+        boolean applyNext = false;
+        // 当前光标内容为空，则直接找前一条历史记录
+        if (buf.getSize() == 0) {
+            applyNext = true;
+        }
+        // 当前光标在行尾，并且行的内容和当前的历史记录一致，说明刚翻到当前的记录，因此直接再向前翻
+        if (cursor == buf.getSize() && curr >= 0 && LineBufferUtils.equals(buf, history.get(curr))) {
+            int next = curr + 1;
+            if (next < history.size()) {
+                applyNext = true;
             }
-            if (LineBufferUtils.matchBeforeCursor(buf, line)) {
-                interaction.refresh(new LineBuffer().insert(line).setCursor(cursor));
-                interaction.setHistoryIndex(i);
-                break;
+        }
+
+        if (applyNext) {
+            // 向前翻一条记录，光标设置为行尾
+            int next = curr + 1;
+            if (next < history.size()) {
+                int[] nextHistory = history.get(next);
+                interaction.refresh(new LineBuffer().insert(nextHistory));
+                interaction.setHistoryIndex(next);
+            }
+        } else {
+            // 获取当前行首到光标的内容，在历史记录里查找匹配的。光标还是当前位置
+            int searchStart = curr + 1;
+            for (int i = searchStart; i < history.size(); ++i) {
+                int[] line = history.get(i);
+                if (LineBufferUtils.equals(buf, line)) {
+                    continue;
+                }
+                if (LineBufferUtils.matchBeforeCursor(buf, line)) {
+                    interaction.refresh(new LineBuffer().insert(line).setCursor(cursor));
+                    interaction.setHistoryIndex(i);
+                    break;
+                }
             }
         }
 
--- src/main/java/io/termd/core/readline/functions/HistorySearchForward.java
+++ src/main/java/io/termd/core/readline/functions/HistorySearchForward.java
@@ -27,21 +27,32 @@ public class HistorySearchForward implements Function {
 
         List<int[]> history = interaction.history();
 
-        int curr = interaction.getHistoryIndex();
+        int currentHistoryIndex = interaction.getHistoryIndex();
 
-        int searchStart = curr - 1;
+        if (currentHistoryIndex == 0 && LineBufferUtils.equals(buf, history.get(currentHistoryIndex))) {
+            // 当前索引为0，说明上一个输入是一个空行里输入一个UP。所以重新设置为空行
+            interaction.refresh(new LineBuffer());
+            interaction.setHistoryIndex(-1);
+        } else if (currentHistoryIndex > 0 && LineBufferUtils.equals(buf, history.get(currentHistoryIndex))
+                        && cursor == buf.getSize()) {
+            // 当前索引大于0，并且当前行的内容和当前history index一致，说明是输入多个UP翻页得到的。所以直接返回下一条history，光标设置为行尾。
+            interaction.refresh(new LineBuffer().insert(history.get(currentHistoryIndex - 1)));
+            interaction.setHistoryIndex(currentHistoryIndex - 1);
+        } else {
+            // 找到当前光标内容 和 历史记录 里匹配的项，光标仍然设置为当前的位置
+            int searchStart = currentHistoryIndex - 1;
+            for (int i = searchStart; i >= 0; --i) {
+                int[] line = history.get(i);
 
-        for (int i = searchStart; i >= 0; --i) {
-            int[] line = history.get(i);
+                if (LineBufferUtils.equals(buf, line)) {
+                    continue;
+                }
 
-            if (LineBufferUtils.equals(buf, line)) {
-                continue;
-            }
-
-            if (LineBufferUtils.matchBeforeCursor(buf, line)) {
-                interaction.refresh(new LineBuffer().insert(line).setCursor(cursor));
-                interaction.setHistoryIndex(i);
-                break;
+                if (LineBufferUtils.matchBeforeCursor(buf, line)) {
+                    interaction.refresh(new LineBuffer().insert(line).setCursor(cursor));
+                    interaction.setHistoryIndex(i);
+                    break;
+                }
             }
         }
 
